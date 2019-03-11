dd8072efb5f93f03b1264705b6a4c74412fe5e3a
Inan Batra <inanbatra@gmail.com>
2019-03-10 23:14:30 -0400

    commit1

---
+++ app/src/main/java/com/gitschwifty/cs2340/gatech/space_trader/Model/MarketPlace.java
@@ -0,0 +1,13 @@
+package com.gitschwifty.cs2340.gatech.space_trader.Model;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MarketPlace extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_market_place);
+    }
+}
