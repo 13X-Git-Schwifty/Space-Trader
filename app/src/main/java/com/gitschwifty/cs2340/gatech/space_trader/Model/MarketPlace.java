1255dacc25db67485778a218dc3d1aca0bf7e424
Riya Agrawal <43094144+ragrawal4545@users.noreply.github.com>
2019-03-11 12:44:55 -0400

    Fixed dependencies
    Fixed dependencies

--- app/src/main/java/com/gitschwifty/cs2340/gatech/space_trader/Model/MarketPlace.java
+++ app/src/main/java/com/gitschwifty/cs2340/gatech/space_trader/Model/MarketPlace.java
@@ -3,6 +3,8 @@ package com.gitschwifty.cs2340.gatech.space_trader.Model;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.gitschwifty.cs2340.gatech.space_trader.R;
+
 public class MarketPlace extends AppCompatActivity {
 
     @Override
