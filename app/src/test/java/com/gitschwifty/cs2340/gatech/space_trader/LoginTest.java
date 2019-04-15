9c3ecdca8ea29b8505e0a720789fea46e5c59433
Inan Batra <inanbatra@gmail.com>
2019-04-14 23:23:18 -0400

    junit

--- app/src/test/java/com/gitschwifty/cs2340/gatech/space_trader/LoginTest.java
+++ app/src/test/java/com/gitschwifty/cs2340/gatech/space_trader/LoginTest.java
@@ -14,16 +14,36 @@ import static org.junit.Assert.assertEquals;
 
 //JUnit written by Inan Batra
 
-class LoginTest {
+public class LoginTest {
+
+    Coordinate Coordinate1;
+    Coordinate Coordinate2;
+    Coordinate Coordinate3;
+    Coordinate Coordinate4;
+    CargoHold cargoHold;
+    Coordinate nullCo;
+    Coordinate Coordinate5;
+    Coordinate Coordinate6;
 
     @Before
     public void setUp() {
-
+        //set of coordinates
+        int list[] = {1, 0, -2, 12, 14, 594595149};
+        Coordinate1 = new Coordinate(list[0], list[1]);
+        Coordinate2 = new Coordinate(list[2], list[3]);
+        Coordinate3 = new Coordinate(list[4], list[5]);
+        //to compare
+        Coordinate4 = new Coordinate(list[0], list[1]);
+        cargoHold = new CargoHold();
+        nullCo = null;
+        Coordinate5 = new Coordinate(list[0], list[2]);
+        Coordinate6 = new Coordinate(list[1], list[2]);
     }
 
     @Test
-    public void skillpoints() {
+    public void testThisOrInstanceOf() {
         assertEquals(true, Coordinate1.equals(Coordinate1));
+        assertEquals(false, Coordinate1.equals(cargoHold));
     }
 
 
