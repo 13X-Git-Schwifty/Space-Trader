ca0c16eafb42eec473640aa5522ed9ef0f89fc95
Inan Batra <inanbatra@gmail.com>
2019-04-14 23:09:15 -0400

    comment

--- app/src/test/java/com/gitschwifty/cs2340/gatech/space_trader/LoginTest.java
+++ app/src/test/java/com/gitschwifty/cs2340/gatech/space_trader/LoginTest.java
@@ -1,36 +1,30 @@
 package com.gitschwifty.cs2340.gatech.space_trader;
 
-// --Commented out by Inspection START (4/13/19, 5:16 PM):
-// --Commented out by Inspection START (4/13/19, 5:16 PM):
-//////JUnit written by Inan Batra
-////class LoginTest {
-////
-////    @Before
-////    public void setUp() {
-////
-////    }
-////
-////
-//////    @Test
-//////    public void testSkillPoints() {
-//////        assertTrue(pilot + fighter + trader + engineer == 16)
-//////    }
-//////
-//////    @Test
-//////    public void testOther() {
-//////        assertEquals(true, Coordinate1.equals(Coordinate4));
-//////        assertEquals(false, Coordinate1.equals(Coordinate5));
-//////        assertEquals(false, Coordinate1.equals(Coordinate6));
-//////    }
-//////
-//////    @Test
-//////    public void testEqualsFalse() {
-//////        assertEquals(false, Coordinate1.equals(Coordinate2));
-//////        assertEquals(false, Coordinate1.equals(Coordinate3));
-// --Commented out by Inspection STOP (4/13/19, 5:16 PM)
-////        assertEquals(false, Coordinate1.equals(nullCo));
-////
-////    }
-//
-//}
-// --Commented out by Inspection STOP (4/13/19, 5:16 PM)
+
+import com.gitschwifty.cs2340.gatech.space_trader.Model.CargoHold;
+import com.gitschwifty.cs2340.gatech.space_trader.Model.Coordinate;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+//JUnit written by Inan Batra
+
+class LoginTest {
+
+    @Before
+    public void setUp() {
+
+    }
+
+    @Test
+    public void skillpoints() {
+        assertEquals(true, Coordinate1.equals(Coordinate1));
+    }
+
+
+}
